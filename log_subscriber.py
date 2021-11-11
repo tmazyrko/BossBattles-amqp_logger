ba3c025f59bd5368a2e0c492165b164c358291f9
tmazyrko <tmazyrko@gmail.com>
2021-11-10 22:12:10 -0500

    Updated to write logs to a file identified by command-line argument

--- log_subscriber.py
+++ log_subscriber.py
@@ -4,14 +4,22 @@ import pika
 from dotenv import dotenv_values
 import signal
 import sys
+import argparse
 
 
 def signal_handler(signal, frame):  # Graceful CTRL+C handler
     print(" [*] Exiting log_subscriber.py")
+    f.write("### STOPPED LOGGING ###\n")
+    f.close()
     sys.exit(0)
 
 
 signal.signal(signal.SIGINT, signal_handler)
+
+parser = argparse.ArgumentParser(description="Subscribes to log messages sent via RabbitMQ.")
+parser.add_argument('output', action="store", type=argparse.FileType('a'), help="Name of output file")
+args = parser.parse_args()
+f = args.output
 
 cfg = dotenv_values(".env")
 
@@ -27,10 +35,12 @@ queue_name = queue.method.queue
 channel.queue_bind(exchange='logs', queue=queue_name)
 
 print(' [*] Waiting for logs. Press CTRL+C to exit.')
+f.write("### STARTED LOGGING ###\n")
 
 
 def callback(ch, method, properties, body):
     msg = body.decode("utf-8")
+    f.write(msg + '\n')
     print(msg)
 
 
