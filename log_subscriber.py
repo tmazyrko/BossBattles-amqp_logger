23fee89ea8db36c1506361024595496c7c57fa2f
tmazyrko <tmazyrko@gmail.com>
2021-11-10 21:25:51 -0500

    Initial revision of log subscriber

---
+++ log_subscriber.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pika
+from dotenv import dotenv_values
+import signal
+import sys
+
+
+def signal_handler(signal, frame):  # Graceful CTRL+C handler
+    print(" [*] Exiting log_subscriber.py")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+cfg = dotenv_values(".env")
+
+# Sets up RabbitMQ connection
+connection = pika.BlockingConnection(pika.URLParameters(cfg['AMQP_URL']))
+
+channel = connection.channel()
+channel.exchange_declare(exchange='logs', exchange_type="fanout", durable=True)
+
+queue = channel.queue_declare(queue='', exclusive=True)
+queue_name = queue.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+print(' [*] Waiting for logs. Press CTRL+C to exit.')
+
+
+def callback(ch, method, properties, body):
+    print(str(body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
